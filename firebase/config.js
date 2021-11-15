53e0895bfe0caab7489652a0c7999e74091ad0aa
jianhewang <kuyasda@hotmail.com>
2021-11-14 21:13:21 -0500

    first commit

--- firebase/config.js
+++ firebase/config.js
@@ -1,12 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyCtoSmzIVOVQL1qW7Ys1HgUHyPzJxqcdFA",
-    authDomain: "dmitjavascript.firebaseapp.com",
-    projectId: "dmitjavascript",
-    storageBucket: "dmitjavascript.appspot.com",
-    messagingSenderId: "252515408402",
-    appId: "1:252515408402:web:5d94fe187f8a6a5c5396cb",
-    measurementId: "G-NDQMNME5KX"
-  };
+const firebaseConfig = { 
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, 
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL, 
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID, 
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID, 
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID, }
 
   export default firebaseConfig
